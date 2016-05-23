3b44ecae1824ca9a58b01f3d4853d1eae5a75de6
Anderson <callum.anderson@mendeley.com>
2016-05-23 10:00:00 +0100

    adding test for work

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -1,8 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'The Welcome page', type: :feature do
+  let(:host) { 'http://example.com' }
+
   it 'should tell the visitor a little about the business' do
-    visit 'http://example.com'
-    expect(page).to have_content 'Callum Anderson'
+    visit host
+    expect(page).to have_content('Callum Anderson')
+  end
+
+  it 'should have a link to contact me' do
+    visit host
+    expect(page).to have_link("Get in touch", :href=>"mailto:me@callum-anderson.com")
+  end
+
+  it 'should have a link to see my work' do
+    visit host
+    expect(page).to have_link("See my work", :href=>"/work")
   end
 end
